44b52f0a7c5d2810ef4037010ba1a93f4e547464
rafalencar1997 <rafael@alencar.srv.br>
2023-08-06 08:53:35 -0300

    Basic login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Header from "./components/common/Header";
 import ProjectsContainer from './components/todo/ProjectsContainer';
- 
+import LoginPage from './components/login/LoginPage'; 
+
 function App() {
   return (
     <div className="App">
        <Header></Header>
-       <ProjectsContainer></ProjectsContainer>
+       <LoginPage></LoginPage>
+       {/* <ProjectsContainer></ProjectsContainer> */}
     </div>
   );
 }
